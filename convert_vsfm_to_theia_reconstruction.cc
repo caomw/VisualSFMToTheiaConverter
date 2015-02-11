3970b757842f52a756b9be3141cc3b9278e666ba
Chris Sweeney <cmsweeney@cs.ucsb.edu>
2015-02-11 20:05:22 +0100

    Initial upload

---
+++ convert_vsfm_to_theia_reconstruction.cc
@@ -0,0 +1,149 @@
+// Copyright (C) 2014 The Regents of the University of California (Regents).
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of The Regents or University of California nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Chris Sweeney (cmsweeney@cs.ucsb.edu)
+
+#include <Eigen/Core>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include <theia/theia.h>
+
+#include "util.h"
+#include "DataInterface.h"
+
+DEFINE_string(input_nvm_file, "",
+              "Input NVM file containing the 3D Reconstruction.");
+DEFINE_string(output_theia_reconstruction_file, "",
+              "Output Theia reconstruction file.");
+
+using theia::View;
+using theia::ViewId;
+using theia::Camera;
+using theia::Track;
+using theia::TrackId;
+
+struct VSFMReconstruction {
+  std::vector<CameraT> camera_data;
+  std::vector<Point3D> point_data;
+  std::vector<Point2D> measurements;
+  std::vector<int> point_index;
+  std::vector<int> camera_index;
+  std::vector<string> view_names;
+  std::vector<int> point_colors;
+};
+
+void ConvertVSFMReconstructionToTheiaReconstruction(
+    const VSFMReconstruction& vsfm_reconstruction,
+    theia::Reconstruction* theia_reconstruction) {
+  CHECK_EQ(vsfm_reconstruction.view_names.size(),
+           vsfm_reconstruction.camera_data.size());
+  CHECK_EQ(vsfm_reconstruction.measurements.size(),
+           vsfm_reconstruction.point_index.size());
+  CHECK_EQ(vsfm_reconstruction.point_index.size(),
+           vsfm_reconstruction.camera_index.size());
+
+  // Add all cameras to the reconstruction.
+  for (int i = 0; i < vsfm_reconstruction.camera_data.size(); i++) {
+    const CameraT& vsfm_camera = vsfm_reconstruction.camera_data[i];
+
+    const ViewId view_id =
+        theia_reconstruction->AddView(vsfm_reconstruction.view_names[i]);
+    CHECK_NE(view_id, theia::kInvalidViewId);
+    View* view = theia_reconstruction->MutableView(view_id);
+    view->SetEstimated(true);
+    Camera* camera = view->MutableCamera();
+    camera->SetFocalLength(vsfm_camera.GetFocalLength());
+
+    Eigen::Matrix3f rotation;
+    vsfm_camera.GetMatrixRotation(rotation.data());
+    camera->SetOrientationFromRotationMatrix(rotation.cast<double>());
+
+    Eigen::Vector3f position;
+    vsfm_camera.GetCameraCenter(position.data());
+    camera->SetPosition(position.cast<double>());
+  }
+
+  // Create the track correspondences.
+  std::unordered_map<int, std::vector<std::pair<theia::ViewId, theia::Feature>>>
+      tracks;
+  for (int i = 0; i < vsfm_reconstruction.measurements.size(); i++) {
+    theia::Feature feature(vsfm_reconstruction.measurements[i].x,
+                           vsfm_reconstruction.measurements[i].y);
+    const int img_id = vsfm_reconstruction.camera_index[i];
+    tracks[vsfm_reconstruction.point_index[i]].emplace_back(img_id, feature);
+  }
+
+  // Add all tracks to the reconstruction and set the 3d position.
+  for (int i = 0; i < vsfm_reconstruction.point_data.size(); i++) {
+    Eigen::Vector4f point(vsfm_reconstruction.point_data[i].xyz[0],
+                          vsfm_reconstruction.point_data[i].xyz[1],
+                          vsfm_reconstruction.point_data[i].xyz[2],
+                          1.0);
+    const auto& features = theia::FindOrDie(tracks, i);
+    const TrackId track_id = theia_reconstruction->AddTrack(features);
+    Track* track = theia_reconstruction->MutableTrack(track_id);
+    track->SetEstimated(true);
+    *track->MutablePoint() = point.cast<double>();
+  }
+}
+
+int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  CHECK_GT(FLAGS_input_nvm_file.length(), 0);
+  CHECK_GT(FLAGS_output_theia_reconstruction_file.length(), 0);
+
+  // Read VSFM file.
+  VSFMReconstruction vsfm_reconstruction;
+  CHECK(LoadModelFile(FLAGS_input_nvm_file.c_str(),
+                      vsfm_reconstruction.camera_data,
+                      vsfm_reconstruction.point_data,
+                      vsfm_reconstruction.measurements,
+                      vsfm_reconstruction.point_index,
+                      vsfm_reconstruction.camera_index,
+                      vsfm_reconstruction.view_names,
+                      vsfm_reconstruction.point_colors));
+
+  // Convert to Theia reconstruction.
+  theia::Reconstruction theia_reconstruction;
+  ConvertVSFMReconstructionToTheiaReconstruction(vsfm_reconstruction,
+                                                 &theia_reconstruction);
+
+  // Output Theia reconstruction.
+  CHECK(theia::WriteReconstruction(theia_reconstruction,
+                                   FLAGS_output_theia_reconstruction_file));
+  return 0;
+}
---
+++ export_vsfm_matches.cc
@@ -0,0 +1,171 @@
+#include <theia/theia.h>
+#include <glog/logging.h>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "MatchFile.h"
+#include "FeaturePoints.h"
+
+DEFINE_string(images, "", "Wildcard of images to reconstruct.");
+DEFINE_string(output_match_file, "", "Location of match file to write.");
+
+// Geometric verification options.
+DEFINE_int32(min_num_verified_matches, 30,
+            "Minimum number of matches to be considered an inlier.");
+
+Eigen::Matrix3d GetEigen3x3Matrix(const float mat[3][3]) {
+  Eigen::Matrix3d eigen_mat;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      eigen_mat(i, j) = mat[i][j];
+    }
+  }
+  return eigen_mat;
+}
+
+bool ReadVSFMMatches(const std::string& match_file,
+                     const visual_sfm::FeatureData& sift1,
+                     visual_sfm::MatchFile* base_image,
+                     theia::ImagePairMatch* match) {
+  // Load SIFT location data.
+  visual_sfm::FeatureData sift2;
+  const std::string sift2_file = match_file + ".sift";
+  sift2.ReadSIFTB_LOC(sift2_file.c_str());
+
+  // Retrieve matches.
+  const int feature_count = sift2.getFeatureNum();
+  visual_sfm::Points<int> vsfm_matches;
+  visual_sfm::TwoViewGeometry two_view_geometry;
+  if (!base_image->GetIMatch(match_file.c_str(),
+                             feature_count,
+                             two_view_geometry,
+                             vsfm_matches)) {
+    LOG(INFO) << "Skipping " << match_file << ". No features to match.";
+    return false;
+  }
+
+  const int num_fmatrix_inliers = two_view_geometry.NF;
+  const int num_ematrix_inliers = two_view_geometry.NE;
+
+  const Eigen::Matrix3d fmatrix = GetEigen3x3Matrix(two_view_geometry.F);
+  const Eigen::Matrix3d rotation = GetEigen3x3Matrix(two_view_geometry.R);
+  const Eigen::Vector3d translation =
+      Eigen::Map<Eigen::Vector3f>(two_view_geometry.T).cast<double>();
+
+  const double focal_length1 = two_view_geometry.F1;
+  const double focal_length2 = two_view_geometry.F2;
+
+  if (num_ematrix_inliers < FLAGS_min_num_verified_matches ||
+      num_fmatrix_inliers < FLAGS_min_num_verified_matches) {
+    return false;
+  }
+
+  VLOG(1) << "Match info:"
+          << "\nNum fmatrix inliers = " << num_fmatrix_inliers
+          << "\nNum ematrix inliers = " << num_ematrix_inliers
+          << "\nFMatrix = \n" << fmatrix
+          << "\nRotation = \n" << rotation
+          << "\nTranslation = " << translation.transpose()
+          << "\nFocal lengths: " << focal_length1 << ", " << focal_length2;
+
+  // Convert feature matches to Theia types.
+  match->correspondences.resize(num_fmatrix_inliers);
+  const auto& sift_location1 = sift1.getLocationData();
+  const auto& sift_location2 = sift2.getLocationData();
+  for (int i = 0; i < num_fmatrix_inliers; i++) {
+    match->correspondences[i].feature1 =
+        Eigen::Vector2d(sift_location1[vsfm_matches[0][i]][0],
+                        sift_location1[vsfm_matches[0][i]][1]);
+    match->correspondences[i].feature2 =
+        Eigen::Vector2d(sift_location2[vsfm_matches[1][i]][0],
+                        sift_location2[vsfm_matches[1][i]][1]);
+  }
+
+  // Set params.
+  match->twoview_info.focal_length_1 = focal_length1;
+  match->twoview_info.focal_length_2 = focal_length2;
+  match->twoview_info.position_2 = -rotation.transpose() * translation;
+  const Eigen::AngleAxisd rotation_aa(rotation);
+  match->twoview_info.rotation_2 = rotation_aa.angle() * rotation_aa.axis();
+  match->twoview_info.num_verified_matches = num_fmatrix_inliers;
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Get full image filepaths.
+  std::vector<std::string> image_files;
+  CHECK(theia::GetFilepathsFromWildcard(FLAGS_images, &image_files));
+  std::vector<theia::CameraIntrinsicsPrior> intrinsics(image_files.size());
+
+  // Assign an id to all view names.
+  std::unordered_map<std::string, int> image_name_to_id;
+  std::vector<std::string> pruned_image_files(image_files.size());
+  for (int i = 0; i < image_files.size(); i++) {
+    // Create the pruned image filename and create an image index.
+    std::string filename;
+    CHECK(theia::GetFilenameFromFilepath(image_files[i], false, &filename));
+    pruned_image_files[i] = filename;
+    image_name_to_id[filename] = i;
+  }
+
+  // Read all putatative matches one by one.
+  std::vector<theia::ImagePairMatch> matches;
+  std::unordered_set<std::string> visited_images;
+  for (int i = 0; i < image_files.size(); i++) {
+    // Open the match file (remove the extension).
+    const std::string base_filepath =
+        image_files[i].substr(0, image_files[i].find_last_of("."));
+    const std::string filename1 = pruned_image_files[i];
+    visited_images.insert(filename1);
+
+    LOG(INFO) << "Reading matches for image = " << filename1;
+    visual_sfm::MatchFile match1(base_filepath.c_str());
+    if (!match1.IsValid()) {
+      LOG(INFO) << "Could not load matches for " << base_filepath;
+      continue;
+    }
+
+    // Load the SIFT descriptors.
+    visual_sfm::FeatureData sift1;
+    const std::string sift1_file = base_filepath + ".sift";
+    sift1.ReadSIFTB_LOC(sift1_file.c_str());
+
+    // Inspect all matches.
+    std::vector<std::string> matched_images;
+    match1.GetMatchedImageList(matched_images);
+    for (const std::string& match2 : matched_images) {
+      std::string filename2;
+      CHECK(theia::GetFilenameFromFilepath(match2, false, &filename2));
+
+      // Do not visit a match twice!
+      if (theia::ContainsKey(visited_images, filename2)) {
+        continue;
+      }
+
+      // Read the match information.
+      theia::ImagePairMatch image_pair_match;
+      image_pair_match.image1_index = i;
+      image_pair_match.image2_index =
+          theia::FindOrDie(image_name_to_id, filename2);
+      if (ReadVSFMMatches(match2, sift1, &match1, &image_pair_match)) {
+        LOG(INFO) << "Matched image " << filename1 << " to image " << filename2
+                  << " with " << image_pair_match.correspondences.size()
+                  << " inliers.";
+        matches.emplace_back(image_pair_match);
+      }
+    }
+  }
+
+  CHECK(theia::WriteMatchesAndGeometry(FLAGS_output_match_file,
+                                       pruned_image_files,
+                                       intrinsics,
+                                       matches))
+      << "Could not write match file";
+
+  return 0;
+}
